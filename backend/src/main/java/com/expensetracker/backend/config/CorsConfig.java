834e84b1f294f621469d8edf78ef2819a114d50e
Harsha jain hj <117076934+Alexrdj11@users.noreply.github.com>
2026-07-31 01:43:08 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/expensetracker/backend/config/CorsConfig.java
+++ backend/src/main/java/com/expensetracker/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://expense-tracker-seven-opal.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
